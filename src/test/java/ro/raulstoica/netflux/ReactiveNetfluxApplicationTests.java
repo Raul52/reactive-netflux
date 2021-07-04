d19febad1788e7b7eb93da395afd482baf9b149b
Raul Stoica <Raul.Stoica@cloudflight.io>
2021-07-04 10:09:07 +0300

    Initial commit

---
+++ src/test/java/ro/raulstoica/netflux/ReactiveNetfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.raulstoica.netflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveNetfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
